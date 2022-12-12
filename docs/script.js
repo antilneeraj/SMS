79d10e2eae890c20337372a5ce81a010ba17c939
Ashish Antil <ashishantil.antil07@gmail.com>
2022-12-12 19:29:46 +0530

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -3,7 +3,16 @@ const teacher = document.querySelector('.rectangle1');
 const lights = [...document.querySelectorAll('.light')];
 const base = {height: 405, width: 365};
 
-const getStyle = (elem, prop) => Number(getComputedStyle(elem)[prop].replace('px', ''));
+function input(name, type, placeholder, ...classList){
+  const input = document.createElement('input');
+  input.name=name;
+  input.type=type;
+  input.placeholder=placeholder;
+  input.classList.add(...classList)
+  
+  return input
+}
+
 [teacher, student].forEach(elem => 
   elem.addEventListener('click', e => {
     if(!elem.clicked){
@@ -20,43 +29,22 @@ const getStyle = (elem, prop) => Number(getComputedStyle(elem)[prop].replace('px
         altElem.parentElement.style.zIndex = '5';
         elem.children[2].style.scale = 0;
         elem.children[1].style.scale = .4;
-        altElem.children[2].style.scale = .6;
-        altElem.children[1].style.scale = .6;
+        altElem.children[2].style.scale = altElem.children[1].style.scale = .6;
       })
 
       const form = document.createElement('form');
       form.classList.add('form');
 
+      let input1, input2;
       if(elem===student){
-        const roll = document.createElement('input');
-        roll.type='number';
-        roll.name='roll';
-        roll.placeholder="Roll Number";
-
-        const date = document.createElement('input');
-        date.type='date';
-        date.name='date';
-
-        roll.classList.add('input', 'roll');
-        date.classList.add('input', 'date');
-
-        form.append(roll, date);
+        input1 = input('number', 'roll', 'Roll Number', 'input', 'roll');
+        input2 = input('date', 'date', '', 'input', 'date');
       }else{
-        const uid = document.createElement('input');
-        uid.type='text';
-        uid.name='uid';
-        uid.placeholder='User ID';
-
-        const password = document.createElement('input');
-        password.type='password';
-        password.name='password';
-        password.placeholder='Password';
-
-        uid.classList.add('input');
-        password.classList.add('input');
-
-        form.append(uid, password);
+        input1 = input('text', 'uid', 'User ID', 'input');
+        input2 = input('password', 'password', 'Password', 'input');
       }
+
+      form.append(input1, input2);
 
       const submitParent = document.createElement('button');
       submitParent.classList.add('button');
@@ -79,26 +67,22 @@ const getStyle = (elem, prop) => Number(getComputedStyle(elem)[prop].replace('px
   })
 )
 
-document.addEventListener('mousemove', e => {
+document.addEventListener('mousemove', e => 
   lights.forEach(light => {
     light.style.display = 'block';
-    light.style.left = `${e.clientX - light.parentElement.getBoundingClientRect().left}px`;
-    light.style.top = `${e.clientY - light.parentElement.getBoundingClientRect().top}px`;
+    ['left', 'top'].forEach(dimen => light.style[dimen] = `${e[`client${dimen==='left'?'X':'Y'}`] - light.parentElement.getBoundingClientRect()[dimen]}px`)
   })
-})
+)
 
 
 const params = new URL(window.location).searchParams;
-if(params.get('user')==='teacher'){
-  teacher.click();
+if(params.get('user')==='teacher' && !teacher.click())
   ['uid', 'password'].forEach(param => {
     const elem = [...teacher.querySelectorAll('*')].filter(elem => elem.name===param)[0];
     elem.value=params.get(param) ?? '';
   })
-}else if(params.get('user')==='student'){
-  student.click();
+else if(params.get('user')==='student' && !student.click())
   ['roll', 'date'].forEach(param => {
     const elem = [...student.querySelectorAll('*')].filter(elem => elem.name===param)[0];
-    elem.value=(param==='roll'?params.get(param)??'':params.get('date')?params.get('date').slice(4, 8)+'-'+params.get('date').slice(2, 4)+'-'+params.get('date').slice(0, 2):'');
+    elem.value=(param==='roll'?params.get(param)??'':params.get('date')?`${params.get('date').slice(4, 8)}-${params.get('date').slice(2, 4)}-${params.get('date').slice(0, 2)}`:'');
   })
-};
