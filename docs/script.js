0487ac3185807eab6849aef8b01f4bfbd71ddbe3
Neeraj <neerajantil.antil26@gmail.com>
2022-12-25 22:21:56 +0530

    modified loading animation

--- docs/script.js
+++ docs/script.js
@@ -1,4 +1,4 @@
-
+// Loading Animation Script
 window.onload = e => {
   document.querySelector('.loading').style.opacity = '0';
   setTimeout(e => document.querySelector('.loading').remove(), 400);
