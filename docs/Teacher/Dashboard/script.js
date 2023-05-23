11e26ba1af7f2ab068d1d2ef980c89f97d1cd3c5
Neeraj <neerajantil.antil26@gmail.com>
2023-05-23 19:39:37 +0530

    Update
    Added Temperature to Dashboard and Calendar to Teacher Dashboard

--- docs/Teacher/Dashboard/script.js
+++ docs/Teacher/Dashboard/script.js
@@ -42,3 +42,56 @@ function script(){
 // Sometimes browser runs another script when one script takes time, and therefore, this leads to some errors or unwanted behaviour. To prevent this we can use window.addEventListener('load', function) to run the function only when the page is fully loaded.
 
 window.addEventListener('load', script);
+
+
+// calendar
+
+const daysTag = document.querySelector(".days");
+const currentDate = document.querySelector(".current-date");
+const prevNextIcon = document.querySelectorAll(".icons span");
+
+let currYear = new Date().getFullYear();
+let currMonth = new Date().getMonth();
+
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+const renderCalendar = () => {
+    const date = new Date(currYear, currMonth, 1);
+    let firstDayofMonth = date.getDay();
+    let lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate();
+    let lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay();
+    let lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate();
+
+    let liTag = "";
+
+    for (let i = firstDayofMonth; i > 0; i--) {
+        liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
+    }
+
+    for (let i = 1; i <= lastDateofMonth; i++) {
+        let isToday = i === new Date().getDate() && currMonth === new Date().getMonth() && currYear === new Date().getFullYear() ? "active" : "";
+        liTag += `<li class="${isToday}">${i}</li>`;
+    }
+
+    for (let i = lastDayofMonth; i < 6; i++) {
+        liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`
+    }
+
+    currentDate.innerText = `${months[currMonth]} ${currYear}`;
+    daysTag.innerHTML = liTag;
+};
+
+renderCalendar();
+
+prevNextIcon.forEach(icon => {
+    icon.addEventListener("click", () => {
+        currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
+
+        if (currMonth < 0 || currMonth > 11) {
+            currYear = icon.id === "prev" ? currYear - 1 : currYear + 1;
+            currMonth = currMonth < 0 ? 11 : 0;
+        }
+
+        renderCalendar();
+    });
+});
